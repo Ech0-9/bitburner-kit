8fbb8e221f982b4d118939bce11060acf6de46b0
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-16 22:50:04 -0600

    Update d_cluster.js

--- d_cluster.js
+++ d_cluster.js
@@ -10,23 +10,23 @@ const SERVICE_MAP = [
 ];
 
 export async function main(ns) {
-  const ME = ns.getHostname();
-  while(true){
-    const CONFIG = getConfig(ns);
-    for(var i=0; i < SERVICE_MAP.length; i++){
-      const service = SERVICE_MAP[i];
-      const scriptName = `d_${service.script}.js`;
-      if(ns.scriptRunning(scriptName, ME)){
-        if(!CONFIG[service.conf]){
-          ns.scriptKill(scriptName, ME);
-        }
-      }
-      else {
-        if(CONFIG[service.conf]){
-          ns.run(scriptName, 1);
-        }
-      }
-    }
-    await ns.sleep(CONFIG.daemonInterval);
-  }
+	const ME = ns.getHostname();
+	while(true){
+    	const CONFIG = getConfig(ns);
+    	for(var i=0; i < SERVICE_MAP.length; i++){
+      		const service = SERVICE_MAP[i];
+      		const scriptName = `d_${service.script}.js`;
+      		if(ns.scriptRunning(scriptName, ME)){
+				if(!CONFIG[service.conf]){
+          			ns.scriptKill(scriptName, ME);
+				}
+			}
+			else {
+				if(CONFIG[service.conf]){
+					ns.run(scriptName, 1);
+				}
+			}
+		}
+		await ns.sleep(CONFIG.daemonInterval);
+	}
 }
